d0399b1e2a280a9954549a168cb40e1d4cc7b57d
Edil Medeiros <jose.edil@gmail.com>
2024-07-24 11:39:34 -0300

    Add doc strings

--- rust/src/hash.rs
+++ rust/src/hash.rs
@@ -7,28 +7,33 @@ pub struct Hash {
 }
 
 impl Hash {
+    /// Zero initlized new Hash
     pub fn new() -> Self {
         Hash {
             data: [0; 32],
         }
     }
 
+    /// Build Hash from byte array
     pub fn from_array(array: [u8; 32]) -> Self {
         Hash {
             data: array,
         }
     }
 
+    /// Compute double sha256 hash from raw data
     pub fn hash256(slice: &[u8]) -> Self {
         Hash {
             data: Sha256::digest(Sha256::digest(slice)).into(),
         }
     }
 
+    /// Convert to String, big endian
     pub fn to_string(&self) -> String {
         hex::encode(self.data)
     }
 
+    /// Convert to String, little endian
     pub fn to_le_string(&self) -> String {
         hex::encode(self.reverse_bytes())
     }
@@ -41,12 +46,17 @@ impl Hash {
         buffer
     }
 
+    /// Access the internal buffer
     pub fn as_slice(&self) -> &[u8] {
         &self.data
     }
+
+    /// Access the internal buffer for mutation
     pub fn as_mut_slice(&mut self) -> &[u8] {
         &self.data
     }
+
+
 }
 
 #[cfg(test)]
@@ -56,12 +66,12 @@ mod tests {
     #[test]
     fn test_hash() {
         // Empty data vector
-        let hash = Hash::from_slice(b"abc");
+        let hash = Hash::hash256(b"abc");
         assert_eq!(hash.to_string(), "4f8b42c22dd3729b519ba6f68d2da7cc5b2d606d05daed5ad5128cc03e6c6358");
 
         // Block 853620
         let block_header = hex::decode("0000a324056ffff5fe093b3d797c70bdabf6562401d9ca2578b4020000000000000000003d4c22c742c8ba771df6c3be8f1785536e221d3b68d44eca716040c9999593142d1ea0663a6e03171d1e9a94").unwrap();
-        let hash = Hash::from_slice(&block_header);
+        let hash = Hash::hash256(&block_header);
         assert_eq!(hash.to_le_string(), "00000000000000000000d89e162692967cb3abc15715068d5b5d21937405ce37");
     }
 }
