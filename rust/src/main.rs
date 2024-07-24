c89593e81abb01b1f9f771ba56db1d25f1481093
Edil Medeiros <jose.edil@gmail.com>
2024-07-24 17:31:39 -0300

    "Minimal" running main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,12 @@
-use std::fs;
-use std::io::Read;
+use week5_lib::hash::Hash;
+use week5_lib::block_header::BlockHeader;
+
+use std::fs::File;
 use std::path::Path;
+use std::io::Write;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use serde::Deserialize;
-
-use bitcoin::*;
-
-use week5_lib::block_header::BlockHeader;
 
 
 #[derive(Debug, PartialEq, Deserialize)]
@@ -44,40 +44,79 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Load mempool into memory
     let mempool_dir = Path::new("mempool");
-    let mut mempool_spec = fs::File::open(mempool_dir.join("mempool.json"))?;
+    let mut mempool_spec = File::open(mempool_dir.join("mempool.json"))?;
     let mempool_files:Vec<String> = serde_json::from_reader(mempool_spec)?;
 
     // let mut mempool: Vec<Transaction> = Vec::new();
     // for file in mempool_files {
-    //     let mut tx_file = fs::File::open(mempool_dir.join(file).join(".json"))?;
+    //     let mut tx_file = File::open(mempool_dir.join(file).join(".json"))?;
     //     let tx: Transaction = serde_json::from_reader(tx_file)?;
     // }
 
     let tx_path = "mempool/00000964b698b728022e6d180add7b2c060676e522ab2907f06198af7b2d0b99.json";
-    let tx_file = fs::File::open(tx_path)?;
+    let tx_file = File::open(tx_path)?;
     let tx: Transaction = serde_json::from_reader(tx_file)?;
 
     println!("{tx:?}");
 
+    // Decide which transactions will enter the block
+    let candidate_txids: Vec<Hash> = Vec::new();
 
+    // Build coinbase transaction
+    let coinbase = 0;
+
+
+    ////////////////////////
+    // Build block header //
+    ////////////////////////
     let mut block_header = BlockHeader::empty();
-    block_header.version = 2;
-    println!("{block_header:?}");
-    println!("{:?}", block_header.serialize());
-    println!("{}", block_header.to_string());
+
+    // Version should be at least 4
+    block_header.version = 4;
+
+    // Previous block hash can be any, using the genesis block =]
+    //"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+    block_header.prev_block_hash = Hash::new();
+
+    // Build merkle root from candidate transactions
+    //block_header.merkle_root = MerkleRoot::compute_merkle_root(candidate_txs);
+    block_header.merkle_root = Hash::new();
+
+    // Timestamp with current time
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    block_header.timestamp = timestamp.as_secs() as u32; // 24/07/2024 23h59m59s
+
+    // The difficulty target is `0000ffff00000000000000000000000000000000000000000000000000000000`
+    block_header.target = 0x1f00ffff;
+
+    // Grind block
+    block_header.nonce = 0;
+
+
 
     //println!("{:?}", mempool);
 
     // let mut mempool: Vec<Transaction> = Vec::new();
-    // for file in fs::read_dir(INPUT_PATH)? {
+    // for file in read_dir(INPUT_PATH)? {
     //     println!("Processing file {:?}", file);
-    //     let transaction_data = fs::File::open(file)?;
+    //     let transaction_data = File::open(file)?;
     //     let tx: Transaction = serde_json::from_reader(transaction_data)?;
     //     println!("{:?}", tx);
 
     // }
 
-    let data: [u8; 80] = [0x31; 80];
-    fs::write("out.txt", data)?;
+    // Output solution data
+    let mut output_file = File::create("out.txt")?;
+    output_file.write(&block_header.to_string().as_bytes())?;
+    output_file.write(b"\n")?;
+    output_file.write(coinbase.to_string().as_bytes())?;
+    output_file.write(b"\n")?;
+    candidate_txids.iter().for_each(|txid| {
+        output_file.write(txid.to_string().as_bytes()).unwrap();
+        output_file.write(b"\n").unwrap();
+    });
+
     Ok(())
 }
