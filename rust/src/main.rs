08e665ed7678b5f9fe5dfaabf045975f7b050545
Edil Medeiros <jose.edil@gmail.com>
2024-07-26 10:33:46 -0300

    Fix block merkle root computation and serialization

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -24,8 +24,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Load mempool into memory
     let mempool_dir = Path::new("mempool");
-    let mut mempool_spec = File::open(mempool_dir.join("mempool.json"))?;
-    let mempool_files:Vec<String> = serde_json::from_reader(mempool_spec)?;
+    let mempool_spec = File::open(mempool_dir.join("mempool.json"))?;
+    let _mempool_files:Vec<String> = serde_json::from_reader(mempool_spec)?;
 
     // let mut mempool: Vec<Transaction> = Vec::new();
     // for file in mempool_files {
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // }
 
     let tx_path = "mempool/00000964b698b728022e6d180add7b2c060676e522ab2907f06198af7b2d0b99.json";
-    let tx_file = File::open(tx_path)?;
+    let _tx_file = File::open(tx_path)?;
     //let tx: Transaction = serde_json::from_reader(tx_file)?;
 
     // TODO: Decide which transactions will enter the block
@@ -127,8 +127,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let coinbase_string = hex::encode(coinbase_serialization);
 
     // Add coinbase txid to the block transactions list
-    let coinbase_txid = Hash::from_hex_string(&coinbase.compute_txid().to_string()).unwrap().reverse();
-    candidate_txids.insert(0, coinbase_txid);
+    let coinbase_txid = Hash::from_hex_string(&coinbase.compute_txid().to_string()).unwrap();
+    let mut tx_list: Vec<Hash> = Vec::new();
+    tx_list.push(coinbase_txid.clone().reverse());
+    for hash in candidate_txids {
+        tx_list.push(hash.reverse());
+    }
 
     ////////////////////////
     // Build block header //
@@ -144,7 +148,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     block_header.prev_block_hash = Hash::from_hex_string("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f").unwrap();
 
     // Build merkle root from candidate transactions
-    block_header.merkle_root = MerkleRoot::compute_merkle_root(&candidate_txids);
+    block_header.merkle_root = MerkleRoot::compute_merkle_root(&tx_list);
 
     // Timestamp with current time
     let timestamp = SystemTime::now()
@@ -172,8 +176,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     output_file.write(b"\n")?;
     output_file.write(coinbase_string.as_bytes())?;
     output_file.write(b"\n")?;
-    candidate_txids.iter().for_each(|txid| {
-        output_file.write(txid.to_string().as_bytes()).unwrap();
+    tx_list.iter().for_each(|txid| {
+        output_file.write(txid.to_le_string().as_bytes()).unwrap();
         output_file.write(b"\n").unwrap();
     });
     log::info!("Finished. Bye!");
